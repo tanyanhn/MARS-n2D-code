9c9ef205b446488ba300ba801388e661dd2a5f1e
LiangKaiYi <369331085@qq.com>
2021-12-29 22:58:11 +0800

    add backtrack

--- src/InterfaceTracking/MARS.cpp
+++ src/InterfaceTracking/MARS.cpp
@@ -9,7 +9,8 @@ template <int Dim, int Order>
 void MARS<Dim, Order>::trackInterface(const VectorFunction<Dim> &v, YS &ys, Real StartTime, Real dt, Real EndTime, bool output, string fName, int opstride)
 {
     Real T = StartTime;
-    Real t = dt;
+    int stages = ceil(abs(EndTime - StartTime) / abs(dt));
+    Real k = (EndTime - StartTime) / stages;
     int step = 1;
     if (output == true)
     {
@@ -17,25 +18,15 @@ void MARS<Dim, Order>::trackInterface(const VectorFunction<Dim> &v, YS &ys, Real
         ys.dump(of);
     }
 
-    while ((dt > 0 && T < EndTime) || (dt < 0 && T > EndTime))
+    while (step <= stages)
     {
-        if (abs(EndTime - T) < abs(dt))
-        {
-            t = EndTime - T;
-        }
-        cout << "Step: " << step << "     timestep: " << t << endl;
+        cout << "Step: " << step << "     timestep: " << k << endl;
 
-        timeStep(v, ys, T, t);
+        timeStep(v, ys, T, k);
 
         cout << endl;
-        T += t;
+        T += k;
 
-        if (output == true && T == EndTime)
-        {
-            ofstream of(string(fName + "_End.dat"), ios_base::binary);
-            ys.dump(of);
-            break;
-        }
         if (output == true && step % opstride == 0)
         {
             ostringstream tmps;
