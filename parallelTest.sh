902b024647a996bb4997362c6b88c654deb52b49
ytan@WorkStation <tanyanhn@qq.com>
2025-03-22 19:12:47 +0800

    parallel update.

--- parallelTest.sh
+++ parallelTest.sh
@@ -3,4 +3,6 @@ tests=${1:-"./build/test/TestMARSn2D"}
 echo "提取的测试名称："
 $tests --list-tests | awk '/^      \[/ {print $1}'
 echo "开始运行测试..."
-$tests --list-tests | awk '/^      \[/ {print $1}' | parallel -j4 "$tests "{}"" 1>&1
+# 计算物理核心数
+physical_cores=$(lscpu | awk '/^Core\(s\) per socket:/ {c=$4} /^Socket\(s\):/ {s=$2} END {print c*s}')
+$tests --list-tests | awk '/^      \[/ {print $1}' | parallel -j $physical_cores "$tests "{}"" 1>&1
