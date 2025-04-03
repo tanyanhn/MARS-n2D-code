1da50b079c5660dc38d0999015b3d121b0129855
ytan@WorkStation <tanyanhn@qq.com>
2025-04-03 15:34:36 +0800

    half Graph41 test.

--- test/matlab/multiPhasePlot.m
+++ test/matlab/multiPhasePlot.m
@@ -8,15 +8,15 @@ fillColor = getColor();
 % filedir = "results/CutCell/";
 % filedir = "results/InterfaceGraph/";
 % filedir = "../../results/TrackInterface/Disk4Vortex16/";
-% filedir = "../../results/TrackInterface/Disk5Deformation4/";
-filedir = "../../results/TrackInterface/Graph41VortexT8Order4/";
+filedir = "../../results/TrackInterface/Disk5DeformationT4Order8/";
+% filedir = "../../results/TrackInterface/Graph41VortexT8Order4/";
 % filedir = "../../test/data1/";
 % round = "No4_";
 
 % filename = "spadjor-13.input.dat";
 % filename = "localVolumes.dat";
 % filename = "localYinset.dat";
-filename = "4Circle_grid32_Step2048_c.dat";
+filename = "4Circle_grid32_Step512_f.dat";
 % filename = "4Circle_grid32.dat";
 % filename = "00test.dat";
 % filename = "test.dat";
@@ -31,7 +31,7 @@ volume = false;
 if volume
     dat = readCellVolume(hd);
 else
-    for k = 2:1:7
+    for k = 2:1:2
 
         if ~tensor
             sf = readYinSet(hd);
@@ -41,8 +41,8 @@ else
             if k == 1 
                 continue;
             end
-            for i=2:1:length(vecSf)
-                plotYinSet(vecSf{i}, lineColor, fillColor{k}); hold on
+            for i=2:1:2
+                plotYinSet(vecSf{i}, lineColor, fillColor{i}); hold on
             end
         end
     end
