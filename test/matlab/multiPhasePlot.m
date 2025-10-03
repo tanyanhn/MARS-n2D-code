c3b780a3d7a7b08cf504da1394ee15e45ebff131
ytan@WorkStation <tanyanhn@qq.com>
2025-10-04 00:50:49 +0800

    matlab add multi png generate.

--- test/matlab/multiPhasePlot.m
+++ test/matlab/multiPhasePlot.m
@@ -2,67 +2,76 @@
 close all
 % clear
 % hold on
-figure
 lineColor = '';
 fillColor = getColor();
-dir = "../../results/TrackInterface/";
-Shape = "Disk";
-% Shape = "Graph";
-part = 5;
-% vel = "Vortex";
+dir = "results/";
+% Shape = "Disk";
+Shape = "Graph";
+% part = 5;
+part = 41;
+% vel = 'Vortex';
 vel = "Deformation";
+dynamic = "Dynamic";
 T = 4;
+sh = 256;
 Order = 4;
-filedir = dir + Shape + num2str(part) + vel + "T" + num2str(T) + "Order" + num2str(Order) + "/";
+filedir = dir + Shape + num2str(part) + vel + "T" + num2str(T) + "Order" + num2str(Order)+ dynamic + "/";
 grid = 32;
-plotT = 2 / 4;
-step = T * grid * 8 * plotT;
+steps = 32;
 type = "_c.dat";
 % type = "_f.dat";
-% filename = "4Circle_grid" + num2str(grid) + "_Step" + num2str(step) + type;
-filename = "1Order4_grid" + num2str(grid) + "_Step" + num2str(step) + type;
-if part == 41
-    part = 5;
-end
-% filedir = "results/CutCell/";
-% filedir = "results/InterfaceGraph/";
-% filedir = "../../results/TrackInterface/Disk4VortexT8Order4/";
-% filedir = "../../results/TrackInterface/Disk2DeformationT2Order4/";
-% filedir = "../../results/TrackInterface/Graph41VortexT8Order4/";
-% filedir = "../../test/data1/";
-% round = "No4_";
+figHandles = [];
+for plotI = 0:1:steps
+%     step = T * grid * steps * plotT;
+    step = T * sh * plotI / steps;
+    filename = "1Order4_grid" + num2str(grid) + "_Step" + num2str(step) + type;
+    f = figure;
+    figHandles = [figHandles f];
+    if part == 41
+        part = 5;
+    end
+    % filedir = "results/CutCell/";
+    % filedir = "results/InterfaceGraph/";
+    % filedir = "../../results/TrackInterface/Disk4VortexT8Order4/";
+    % filedir = "../../results/TrackInterface/Disk2DeformationT2Order4/";
+    % filedir = "../../results/TrackInterface/Graph41VortexT8Order4/";
+    % filedir = "../../test/data1/";
+    % round = "No4_";
 
-% filename = "spadjor-13.input.dat";
-% filename = "localVolumes.dat";
-% filename = "localYinset.dat";
-% filename = "4Circle_grid32_Step256_c.dat";
-% filename = "4Circle_grid32.dat";
-% filename = "00test.dat";
-% filename = "test.dat";
-% Order = 4;
-% N = 4;
-% Shape = "Rose";
-% Shape = "Rectangle";
-% filename = num2str(Order) + Shape + num2str(N) + "_" + num2str(k) + ".dat";
-hd = fopen(filedir + filename);
-tensor = true;
-volume = false;
-if volume
-    dat = readCellVolume(hd);
-else
-    for k = 1:1:(part + 1)
+    % filename = "spadjor-13.input.dat";
+    % filename = "localVolumes.dat";
+    % filename = "localYinset.dat";
+    % filename = "4Circle_grid32_Step256_c.dat";
+    % filename = "4Circle_grid32.dat";
+    % filename = "00test.dat";
+    % filename = "test.dat";
+    % Order = 4;
+    % N = 4;
+    % Shape = "Rose";
+    % Shape = "Rectangle";
+    % filename = num2str(Order) + Shape + num2str(N) + "_" + num2str(k) + ".dat";
+    hd = fopen(filedir + filename);
+    tensor = true;
+    volume = false;
+    if volume
+        dat = readCellVolume(hd);
+    else
+        for k = 1:1:(part + 1)
 
-        if ~tensor
-            sf = readYinSet(hd);
-            plotYinSet(sf, lineColor, fillColor{k}); hold on
-        else
-            vecSf = readVecYinSet(hd);
-            if k == part + 1
-                continue;
-            end
-            for i=1:1:length(vecSf)
-                plotYinSet(vecSf{i}, lineColor, fillColor{k}); hold on
+            if ~tensor
+                sf = readYinSet(hd);
+                plotYinSet(sf, lineColor, fillColor{k}); hold on
+            else
+                vecSf = readVecYinSet(hd);
+                if k == part + 1
+                    continue;
+                end
+                for i=1:1:length(vecSf)
+                    plotYinSet(vecSf{i}, lineColor, fillColor{k}); hold on
+                end
             end
         end
     end
+    fclose(hd);
 end
+saveFiguresAsFrames(vel, figHandles);
---
+++ test/matlab/saveFiguresAsFrames.m
@@ -0,0 +1,75 @@
+function saveFiguresAsFrames(outputDir, figHandles, varargin)
+%SAVEFIGURESASFRAMES 将一组 figure 保存为有序 PNG 帧图像
+%
+% 用法：
+%   saveFiguresAsFrames('my_frames', [fig1, fig2, fig3])
+%
+% 可选参数（Name-Value 对）：
+%   'XLim'        - x 轴范围，如 [0 10]（默认：[]，保留原图）
+%   'YLim'        - y 轴范围，如 [0 5]（默认：[]）
+%   'Resolution'  - 输出分辨率（DPI，默认 150）
+%   'BackgroundColor' - 背景色（默认 'white'）
+%   'Prefix'      - 文件名前缀（默认 'frame'）
+%
+% 输出文件：outputDir/frame001.png, frame002.png, ...
+
+    % === 默认参数 ===
+    p = inputParser;
+    addRequired(p, 'outputDir', @ischar);
+    addRequired(p, 'figHandles', @(x) isvector(x) && all(isvalid(x)) && all(strcmp({x.Type}, 'figure')));
+    addParameter(p, 'XLim', [0 1], @(x) isempty(x) || (isnumeric(x) && numel(x)==2));
+    addParameter(p, 'YLim', [0 1], @(x) isempty(x) || (isnumeric(x) && numel(x)==2));
+    addParameter(p, 'Resolution', 150, @(x) isnumeric(x) && x > 0);
+    addParameter(p, 'BackgroundColor', 'white', @ischar);
+    addParameter(p, 'Prefix', 'frame', @ischar);
+    parse(p, outputDir, figHandles, varargin{:});
+
+    xlim_range = p.Results.XLim;
+    ylim_range = p.Results.YLim;
+    resolution = p.Results.Resolution;
+    bg_color   = p.Results.BackgroundColor;
+    prefix     = p.Results.Prefix;
+
+    % === 创建输出目录 ===
+    if ~exist(outputDir, 'dir')
+        mkdir(outputDir);
+    end
+
+    % === 遍历每个 figure ===
+    nFigs = numel(figHandles);
+    for k = 1:nFigs
+        fig = figHandles(k);
+        
+        % 设置背景色
+        fig.Color = bg_color;
+        
+        % 获取第一个 axes（如有）
+        axList = findobj(fig, 'Type', 'axes');
+        if ~isempty(axList)
+            ax = axList(1); % 取第一个坐标轴
+            if ~isempty(xlim_range)
+                xlim(ax, xlim_range);
+            end
+            if ~isempty(ylim_range)
+                ylim(ax, ylim_range);
+            end
+        end
+
+        % 构造文件名
+        filename = fullfile(outputDir, sprintf('%s%03d.png', prefix, k));
+
+        % 保存图像
+        if verLessThan('matlab', '9.8') % R2020a 之前
+            print(fig, '-dpng', ['-r' num2str(resolution)], filename);
+        else
+            exportgraphics(fig, filename, ...
+                'Resolution', resolution, ...
+                'ContentType', 'auto', ...
+                'BackgroundColor', bg_color);
+        end
+
+        fprintf('Saved: %s\n', filename);
+    end
+
+    fprintf('✅ 共 %d 个 figure 已保存到 "%s"\n', nFigs, outputDir);
+end
